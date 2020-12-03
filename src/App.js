9cf9503aa28cc0f8e8e286f9ec94dff0e1f3ffc3
Thomas Zhang <thomaszhang97@gmail.com>
2020-12-03 01:41:16 -0500

    implemented search functionality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Search from './components/Search'
+import axios from 'axios'
 
 function App() {
+  const [state, setState] = useState({
+    movieInput: '',
+    movieResults: [],
+    selectedMovie: {},
+  })
+
+  // Base URL for API calls
+  const apiURL = 'http://www.omdbapi.com/?i=tt3896198&apikey=71b07b4f'
+
+  // handleChange
+  const handleInput = (e) => {
+    let input = e.target.value
+
+    setState((prevState) => {
+      return { ...prevState, movieInput: input }
+    })
+  }
+
+  async function handleEnter(e) {
+    if (e.key === 'Enter') {
+
+      // Returns a nested object, we'll find search results in []data.data.search
+      let data = await axios(`${apiURL}&s=${state.movieInput}`)
+      console.log(data)
+    }
+  }
+
   return (
     <div className='App'>
       <header>
@@ -10,7 +38,7 @@ function App() {
 
       <main>
         {/* Search Component */}
-        <Search />
+        <Search handleInput={handleInput} handleEnter={handleEnter}/>
       </main>
     </div>
   )
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import '../css/search.css'
 
-function Search() {
+function Search({ handleInput, handleEnter }) {
   return (
     <section className='search-box-wrap'>
       <input
         type='text'
         placeholder='Search for a movie...'
         className='search-box'
+        onChange={handleInput}
+        onKeyPress={handleEnter}
       ></input>
     </section>
   )
